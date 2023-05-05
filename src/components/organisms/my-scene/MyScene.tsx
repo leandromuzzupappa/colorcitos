44bb60f24756fe427221373ffc1cdbf7d38a65f8
LMuzzupappa <leandro.muzzupappa@mediamonks.com>
2023-05-05 18:09:30 -0300

    Add lights to the scene

--- src/components/organisms/my-scene/MyScene.tsx
+++ src/components/organisms/my-scene/MyScene.tsx
@@ -3,6 +3,7 @@ import { useFrame } from "@react-three/fiber";
 import { Ball } from "@/components/molecules/ball/Ball";
 import { Floor } from "@/components/molecules/floor/Floor";
 import { Geometry } from "@/components/molecules/geometry/Geometry";
+import { CustomGeometry } from "@/components/molecules/custom-geometry/CustomGeometry";
 
 function updateColor(currentRedValue: number, deltaTime: number): number {
   let newRedValue = currentRedValue + deltaTime;
@@ -14,7 +15,6 @@ export const MyScene = () => {
   const groupRef = useRef<THREE.Group>(null);
 
   useFrame((state, delta) => {
-    console.log(delta);
     cubeRef.current!.material.color.r = updateColor(
       cubeRef.current!.material.color.r,
       delta
@@ -25,10 +25,19 @@ export const MyScene = () => {
 
   return (
     <>
+      <directionalLight
+        position={[1, 2, 3]}
+        color="limegreen"
+        intensity={0.2}
+      />
+      <ambientLight intensity={0.05} />
+
       <group ref={groupRef}>
         <Geometry myRef={cubeRef} />
         <Ball />
       </group>
+
+      <CustomGeometry />
 
       <Floor />
     </>
