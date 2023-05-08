9e70d948acfff8fc78cc59be8247d42154ae0e79
LMuzzupappa <leandro.muzzupappa@mediamonks.com>
2023-05-08 11:52:59 -0300

    test

--- src/components/organisms/my-scene/MyScene.tsx
+++ src/components/organisms/my-scene/MyScene.tsx
@@ -1,6 +1,10 @@
 import { useRef } from "react";
 import { useFrame } from "@react-three/fiber";
-import { OrbitControls, TransformControls } from "@react-three/drei";
+import {
+  OrbitControls,
+  PivotControls,
+  TransformControls,
+} from "@react-three/drei";
 import { MeshBasicMaterial, Object3D } from "three";
 import { Ball } from "@/components/molecules/ball/Ball";
 import { Floor } from "@/components/molecules/floor/Floor";
@@ -12,9 +16,11 @@ export const MyScene = () => {
   const cubeRef = useRef<THREE.Mesh>(null);
   const groupRef = useRef<THREE.Group>(null);
   const customGeometryRef = useRef<THREE.Mesh>(null);
+  const ballRef = useRef<THREE.Mesh>(null);
 
   useFrame((state, delta) => {
-    /* const angle = state.clock.getElapsedTime();
+    const angle = state.clock.getElapsedTime();
+    /*
     state.camera.position.x = Math.sin(angle) + delta * 2;
     state.camera.position.z = Math.cos(angle) + delta * 2;
     state.camera.lookAt(0, 0, 0); */
@@ -22,7 +28,9 @@ export const MyScene = () => {
     const cubeMaterial = cubeRef.current!.material as MeshBasicMaterial;
     cubeMaterial.color.r = updateColor(cubeMaterial.color.r, delta);
 
-    groupRef.current!.rotation.y += delta * 2;
+    groupRef.current!.rotation.y += delta * 0.0004;
+
+    ballRef.current!.rotation.y = angle * 2;
   });
 
   return (
@@ -37,8 +45,11 @@ export const MyScene = () => {
       <OrbitControls makeDefault />
 
       <group ref={groupRef}>
-        <Geometry myRef={cubeRef} />
-        <Ball />
+        <PivotControls anchor={[0, 0, 0]} scale={1.5} depthTest={false}>
+          <Geometry myRef={cubeRef} />
+        </PivotControls>
+
+        <Ball myRef={ballRef} />
       </group>
 
       <TransformControls object={customGeometryRef.current as Object3D} />
