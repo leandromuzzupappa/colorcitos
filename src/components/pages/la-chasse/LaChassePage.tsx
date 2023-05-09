c1a8418a548ca308a096825f4ca9b869f609b142
LMuzzupappa <leandro.muzzupappa@mediamonks.com>
2023-05-09 02:22:29 -0300

    Update LaChasse page with lights and model

--- src/components/pages/la-chasse/LaChassePage.tsx
+++ src/components/pages/la-chasse/LaChassePage.tsx
@@ -1,13 +1,30 @@
+import { useState } from "react";
 import { Canvas } from "@react-three/fiber";
+import {
+  OrbitControls,
+  PerformanceMonitor,
+  SoftShadows,
+} from "@react-three/drei";
 import { LaChasseModel } from "@/components/molecules/la-chasse-model/Model";
 import styles from "./LaChassePage.module.scss";
 
 export const LaChassePage = () => {
+  const [enabled, setEnabled] = useState(true);
+
   return (
     <div className={styles.pepitos}>
-      <Canvas>
-        <ambientLight intensity={0.8} />
+      <Canvas shadows="basic" camera={{ position: [0, 1.5, 7], fov: 45 }}>
+        <OrbitControls makeDefault />
+
+        <fog attach="fog" args={["black", 0, 1000]} />
+
+        <pointLight position={[10, -10, -20]} intensity={2} />
+        <pointLight position={[-10, -10, -20]} intensity={2} />
+
         <LaChasseModel />
+        {enabled && <SoftShadows />}
+
+        <PerformanceMonitor onDecline={() => setEnabled(false)} />
       </Canvas>
     </div>
   );
