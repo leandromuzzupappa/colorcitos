569d76994fe1d4ec60357571473b73b7e7e06752
LMuzzupappa <leandro.muzzupappa@mediamonks.com>
2023-05-11 14:06:46 -0300

    update floor component allowing to pass props to the material

--- src/components/molecules/floor/Floor.tsx
+++ src/components/molecules/floor/Floor.tsx
@@ -1,8 +1,16 @@
-export const Floor = () => {
+import { MeshReflectorMaterial } from "@react-three/drei";
+
+interface IFloorProps {
+  mirror: number;
+  [key: string]: any;
+}
+
+export const Floor = ({ mirror = 0.5, ...props }: IFloorProps) => {
   return (
     <mesh rotation-x={-Math.PI * 0.5} position-y={-1} scale={10}>
       <planeBufferGeometry />
-      <meshStandardMaterial color="grey" />
+      {/* <meshStandardMaterial color="grey" /> */}
+      <MeshReflectorMaterial mirror={mirror} {...props} />
     </mesh>
   );
 };
