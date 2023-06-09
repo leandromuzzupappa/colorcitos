2b4da695448f51f9a2211077e941a597f164c9d1
LMuzzupappa <leandro.muzzupappa@mediamonks.com>
2023-06-09 15:34:58 -0300

    Update interfaces with ModelProps

--- src/data/interfaces.ts
+++ src/data/interfaces.ts
@@ -5,3 +5,18 @@ export interface IRefMesh {
 export interface IGeometryProps extends IRefMesh {}
 export interface ICustomGeometryProps extends IRefMesh {}
 export interface IBallProps extends IRefMesh {}
+
+export interface IModelProps {
+  position?: THREE.Vector3 | number[];
+  "position-x"?: number;
+  "position-y"?: number;
+  "position-z"?: number;
+  rotation?: THREE.Euler | number[];
+  "rotation-x"?: number;
+  "rotation-y"?: number;
+  "rotation-z"?: number;
+  scale?: THREE.Vector3 | number[];
+  "scale-x"?: number;
+  "scale-y"?: number;
+  "scale-z"?: number;
+}
