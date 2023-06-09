ace5d448a0d55b6f7d1129750259bcae2c614b56
LMuzzupappa <leandro.muzzupappa@mediamonks.com>
2023-06-09 16:59:56 -0300

    Test new properties to interface IModelProps

--- src/data/interfaces.ts
+++ src/data/interfaces.ts
@@ -8,4 +8,8 @@ export interface IGeometryProps extends IRefMesh {}
 export interface ICustomGeometryProps extends IRefMesh {}
 export interface IBallProps extends IRefMesh {}
 
-export interface IModelProps extends GroupProps {}
+export interface IModelProps extends GroupProps {
+  positionX?: number;
+  positionY?: number;
+  positionZ?: number;
+}
