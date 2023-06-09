17fd9f4760956ceb926892bfe79b7eb72bb8177f
LMuzzupappa <leandro.muzzupappa@mediamonks.com>
2023-06-09 16:52:05 -0300

    Add clouds and a line along the scene

--- src/components/organisms/scenes/atmos-scene/Scene.tsx
+++ src/components/organisms/scenes/atmos-scene/Scene.tsx
@@ -1,8 +1,43 @@
-import { OrbitControls } from "@react-three/drei";
+import * as THREE from "three";
+import { useMemo } from "react";
+import { Float, Line, OrbitControls } from "@react-three/drei";
 import { Background } from "@/components/molecules/backgrounds/atmos-background/Background";
 import { Airplane2 } from "@/components/molecules/airplane2/Airplane2";
+import { Cloud } from "@/components/molecules/cloud/Cloud";
+
+const linePointsCount = 2000;
 
 export const Scene = () => {
+  const curve = useMemo(() => {
+    return new THREE.CatmullRomCurve3(
+      [
+        new THREE.Vector3(0, 0, 0),
+        new THREE.Vector3(0, 0, -10),
+        new THREE.Vector3(-2, 0, -20),
+        new THREE.Vector3(-3, 0, -30),
+        new THREE.Vector3(0, 0, -40),
+        new THREE.Vector3(5, 0, -50),
+        new THREE.Vector3(7, 0, -60),
+        new THREE.Vector3(5, 0, -70),
+      ],
+      false,
+      "catmullrom",
+      0.5
+    );
+  }, []);
+
+  const linePoints = useMemo(() => {
+    return curve.getPoints(linePointsCount);
+  }, [curve]);
+
+  const shape = useMemo(() => {
+    const shape = new THREE.Shape();
+    shape.moveTo(0, -0.2);
+    shape.lineTo(0, 0.2);
+
+    return shape;
+  }, []);
+
   return (
     <>
       <ambientLight intensity={1} />
@@ -10,7 +45,57 @@ export const Scene = () => {
 
       <Background />
 
-      <Airplane2 rotation={[0, Math.PI, 0]} />
+      <Float floatIntensity={2} speed={2}>
+        <Airplane2
+          rotation={[0, Math.PI, 0]}
+          scale={[0.2, 0.2, 0.2]}
+          position-y={0.1}
+        />
+      </Float>
+
+      <group position-y={-1}>
+        {/* <Line
+          points={linePoints}
+          color={"white"}
+          opacity={0.7}
+          transparent
+          lineWidth={16}
+        /> */}
+
+        <mesh>
+          <extrudeGeometry
+            args={[
+              shape,
+              {
+                steps: linePointsCount,
+                bevelEnabled: false,
+                extrudePath: curve,
+              },
+            ]}
+          />
+
+          <meshStandardMaterial color="white" opacity={0.7} transparent />
+        </mesh>
+      </group>
+
+      <Cloud opacity={0.5} scale={[0.3, 0.3, 0.3]} position={[-2, 1, -3]} />
+      <Cloud opacity={0.5} scale={[0.2, 0.3, 0.4]} position={[1.5, -0.5, -2]} />
+
+      <Cloud
+        opacity={0.7}
+        scale={[0.3, 0.3, 0.4]}
+        position={[2, -0.2, -2]}
+        rotation-y={Math.PI / 9}
+      />
+      <Cloud
+        opacity={0.7}
+        scale={[0.4, 0.4, 0.4]}
+        position={[1, -0.2, -12]}
+        rotation-y={Math.PI / 9}
+      />
+
+      <Cloud opacity={0.7} scale={[0.5, 0.5, 0.5]} position={[-1, 1, -53]} />
+      <Cloud opacity={0.3} scale={[0.8, 0.8, 0.8]} position={[0, 1, -100]} />
     </>
   );
 };
