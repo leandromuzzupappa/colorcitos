eaad77c8faa45cc6149b92c0baefd38d8fd3b7dc
LMuzzupappa <leandro.muzzupappa@mediamonks.com>
2023-06-09 15:40:05 -0300

    Update IModelProps interface

--- src/data/interfaces.ts
+++ src/data/interfaces.ts
@@ -1,3 +1,5 @@
+import { GroupProps } from "@react-three/fiber";
+
 export interface IRefMesh {
   myRef: React.MutableRefObject<THREE.Mesh | null>;
 }
@@ -6,17 +8,4 @@ export interface IGeometryProps extends IRefMesh {}
 export interface ICustomGeometryProps extends IRefMesh {}
 export interface IBallProps extends IRefMesh {}
 
-export interface IModelProps {
-  position?: THREE.Vector3 | number[];
-  "position-x"?: number;
-  "position-y"?: number;
-  "position-z"?: number;
-  rotation?: THREE.Euler | number[];
-  "rotation-x"?: number;
-  "rotation-y"?: number;
-  "rotation-z"?: number;
-  scale?: THREE.Vector3 | number[];
-  "scale-x"?: number;
-  "scale-y"?: number;
-  "scale-z"?: number;
-}
+export interface IModelProps extends GroupProps {}
