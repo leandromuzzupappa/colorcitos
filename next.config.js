2bb748d83b523ec4b57fb69c88887a88799e2fe6
LMuzzupappa <leandro.muzzupappa@mediamonks.com>
2023-06-09 17:18:46 -0300

    Test config allowed domains for images

--- next.config.js
+++ next.config.js
@@ -6,6 +6,9 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
+  images: {
+    domains: ["market-assets.fra1.cdn.digitaloceanspaces.com"],
+  },
 };
 
 module.exports = nextConfig;
